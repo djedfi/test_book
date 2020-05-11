f10bcda9eca59a7b13aff89b0ed51911cfe38ba7
Edwin Figueroa <fh040234@gmail.com>
2020-05-10 20:39:06 -0400

    Fix the year
    Fix the year.

--- index_book.php
+++ index_book.php
@@ -117,7 +117,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; EdFi 2019</span>
+            <span>Copyright &copy; EdFi 2020</span>
           </div>
         </div>
       </footer>
